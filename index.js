aecfd2865fe65f40c1a35ea6306775638c04db59
kksrini89 <kksrini89@gmail.com>
2020-03-13 16:58:53 +0530

    Left sidebar toggling is done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-document.addEventListener('load', function () {
-   console.log(`App is loaded...`);    
+const leftSidebar = document.querySelector('.left-sidebar');
+const sidebarToggleLink = document.querySelector('.sidebar-toggle');
+
+document.addEventListener('load', function() {
+  console.log(`App is loaded...`);
+});
+
+if (sidebarToggleLink) {
+  sidebarToggleLink.addEventListener('click', function(e) {
+    e.stopPropagation();
+    if (leftSidebar) {
+      leftSidebar.classList.toggle('minimize-menu');
+    }
+  });
+}
+
+document.body.addEventListener('click', function() {
+  if (sidebarToggleLink) {
+    leftSidebar.classList.toggle('minimize-menu');
+  }
 });
